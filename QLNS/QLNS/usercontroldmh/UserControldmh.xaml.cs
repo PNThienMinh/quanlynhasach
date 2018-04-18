d98e6cd4d816efac3f4b266c96150a3b5cbc035d
minhnd239 <37163661+minhnd239@users.noreply.github.com>
2018-04-18 07:54:27 +0700

    loginscreen
    Loginscreen

---
+++ QLNS/QLNS/usercontroldmh/UserControldmh.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QLNS.usercontroldmh
+{
+    /// <summary>
+    /// Interaction logic for UserControldmh.xaml
+    /// </summary>
+    public partial class UserControldmh : UserControl
+    {
+        public UserControldmh()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
